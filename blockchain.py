200a4f5a91d9a3d44d9fb42bb9dc731a02caf424
hondo-s <shunsuke.hondo@gmail.com>
2018-11-18 17:55:28 +0900

    Consensus

--- blockchain.py
+++ blockchain.py
@@ -1,39 +1,100 @@
+import hashlib
+import json
 from time import time
-
-# Genesis block
-init_tx = [
-    {
-        "sender": None,
-        "recipient": "localhost:2000",
-        "amount": 1000
-    },
-    {
-        "sender": None,
-        "recipient": "localhost:2001",
-        "amount": 2000
-    }
-]
+import requests
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
-        self.current_txs = []
+        self.current_transactions = []
         self.nodes = set()
-
-        self.current_txs.extend(init_tx)
 
         self.new_block(previous_hash=1, proof=100)
     
-    def new_block(self, previous_hash, proof):
+    def register_node(self, address):
+        self.nodes.add(address)
+        
+    def new_block(self, proof, previous_hash=None):
         block = {
             "index": len(self.chain) + 1,
             "timestamp": time(),
-            "transactions": self.current_txs,
+            "transactions": self.current_transactions,
             "proof": proof,
-            "previous_hash": previous_hash
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
         }
 
-        self.current_txs = []
+        self.current_transactions = []
         self.chain.append(block)
         return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        })
+
+        return self.last_block["index"] + 1
+    
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        
+        return proof
+    
+    def resolve_conficts(self):
+        new_chain = None
+        max_lengh = len(self.chain)
+
+        for node in self.nodes:
+            res = requests.get(f"http://{node}/chain")
+            if res.status_code == 200:
+                length = res.json()["length"]
+                chain = res.json()["chain"]
+
+                if length > max_lengh and self.valid_chain(chain):
+                    max_lengh = length
+                    new_chain = chain
+            
+        if new_chain:
+            self.chain = new_chain
+            return True
+        
+        return False
+    
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block["previous_hash"] != self.hash(last_block):
+                return False
+            
+            if not self.valid_proof(last_block["proof"], block["proof"]):
+                return False
+            
+            last_block = block
+            current_index += 1
+        
+        return True
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
--- index.py
+++ index.py
@@ -1,53 +1,90 @@
-from bottle import (
-    run,
-    request,
-    response,
-    post,
-    get,
-    abort
-)
+from bottle import route, run, abort
+from bottle import post, get, put, delete, request, response
 import json
+from uuid import uuid4
 
 from blockchain import Blockchain
-blockchain = None
 
-nodes = set()
+node_id = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@post("/nodes/register")
+def register_node():
+    values = request.json
+    nodes = values.get("nodes")
+
+    for node in nodes:
+        blockchain.register_node(node)
+    
+    return ""
+
+
+@get("/nodes/resolve")
+def consensus():
+    replaced = blockchain.resolve_conficts()
+
+    if replaced:
+        return "Replaced"
+    else:
+        return "Not replaced"
+
+@post("/transactions/new")
+def new_transaction():
+    values = request.json
+    required = ["sender", "recipient", "amount"]
+    if not all(k in values for k in required):
+        abort(400, "Wrong parameter")
+    
+    index = blockchain.new_transaction(
+        values["sender"],
+        values["recipient"],
+        values["amount"]
+    )
+
+    res_data = {
+        "message": f"トランザクションはブロック{index}に追加されました。"
+    }
+
+    return json.dumps(res_data)
+
+@get("/mine")
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block["proof"]
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_id,
+        amount=1
+    )
+
+    block = blockchain.new_block(proof)
+
+    res_data = {
+        "message": "新しいブロックを採掘しました",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"]
+    }
+
+    return json.dumps(res_data)
+
 
 @get("/chain")
-def chain():
+def full_chain():
+    response.set_header("content-type", "application/json")
     res_data = {
         "chain": blockchain.chain,
         "length": len(blockchain.chain)
     }
 
-    response.set_header(
-        "content-type", "application/json"
-    )
     return json.dumps(res_data)
-
-@post("/nodes/register")
-def node_register():
-    data = request.json
-    _nodes = data["nodes"]
-    for node in _nodes:
-        nodes.add(node)
-
-    res_data = {
-        "nodes": list(nodes)
-    }
-    response.set_header(
-        "content-type",
-        "application/json"
-    )
-
-    return json.dumps(res_data)
-
-
-def main():
-    global blockchain
-    blockchain = Blockchain()
-    run(port=2000, debug=True, reloader=True)
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    port = sys.argv[1]
+    run(host="localhost", port=port, debug=True, reloader=True)
